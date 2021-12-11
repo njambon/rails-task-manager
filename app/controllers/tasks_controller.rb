b6835383e08f9ae01c5759751a133cb988a63d41
nicolas jambon <nicolas_jambon@hotmail.fr>
2021-12-11 12:57:44 +0100

    Challenge 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
+
+end
